d45ddfd8b2d7dd1c646b1cb01bf1e4f020e6fdd6
kherma <kamil.herma01@gmail.com>
2021-04-27 22:24:39 +0200

    Update LoginBox styles

--- src/components/common/LoginBox/LoginBox.js
+++ src/components/common/LoginBox/LoginBox.js
@@ -6,9 +6,8 @@ const LoginBox = () => {
     <div className={styles.root}>
       <div className={styles.screen}>
         <p className={styles.text}>are you ready?</p>
-        <button className={styles.buttonLogin}>journey begins</button>
       </div>
-      <button className={styles.buttonSwitch}></button>
+      <button className={styles.buttonLogin}>journey begins</button>
     </div>
   );
 };
