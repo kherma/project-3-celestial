a454c30c6b79b94ee5452fd8bf8043904464bd54
kherma <kamil.herma01@gmail.com>
2021-04-28 01:25:43 +0200

    Emphasis LandingLayout text

--- src/components/common/LoginBox/LoginBox.js
+++ src/components/common/LoginBox/LoginBox.js
@@ -5,7 +5,9 @@ const LoginBox = () => {
   return (
     <div className={styles.root}>
       <div className={styles.screen}>
-        <p className={styles.text}>are you ready?</p>
+        <p className={styles.text}>
+          are you <span className={styles.textSpecial}> ready </span>?
+        </p>
       </div>
       <button className={styles.buttonLogin}>journey begins</button>
     </div>
--- src/components/views/HomePage/HomePage.js
+++ src/components/views/HomePage/HomePage.js
@@ -17,7 +17,7 @@ const HomePage = () => {
             <p className={styles.terminalText}>
               With the year 2021, the earth and all intelligent creatures that
               inhabit it gain access to the imaginary interdimensional
-              celestials auction.
+              <span className={styles.logoText}> celestials </span>auction.
             </p>
             <span className={styles.terminalGrid}></span>
           </div>
