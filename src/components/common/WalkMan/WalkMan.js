5684a57bba906d4200e2253ae230f3c0977797ea
kherma <kamil.herma01@gmail.com>
2021-04-28 17:35:29 +0200

    Add music to WalkMan component

--- src/components/common/WalkMan/WalkMan.js
+++ src/components/common/WalkMan/WalkMan.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import styles from './WalkMan.module.scss';
+import useSound from 'use-sound';
+import sounds from '../../../resources/hello.mp3';
 
 const WalkMan = () => {
+  const [play, { stop }] = useSound(sounds, {
+    volume: 0.5,
+    interrupt: false,
+  });
+
+  const stopSound = () => {
+    stop();
+  };
+
+  const playSound = () => {
+    play();
+  };
+
   return (
     <div className={styles.root}>
       <div className={styles.walkMan}>
         <span className={styles.switch}></span>
         <div className={styles.controllers}>
-          <button></button>
-          <button></button>
+          <button onClick={stopSound}></button>
+          <button onClick={playSound}></button>
         </div>
       </div>
     </div>
