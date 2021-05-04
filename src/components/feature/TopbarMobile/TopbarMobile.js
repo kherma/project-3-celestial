eea14049f58c4cd95c3f49514c80b20650b5cbf8
kherma <kamil.herma01@gmail.com>
2021-05-04 14:09:21 +0200

    Add mobile Topbar

--- src/components/feature/TopbarMobile/TopbarMobile.js
+++ src/components/feature/TopbarMobile/TopbarMobile.js
@@ -1,16 +1,50 @@
 import React, { useState } from 'react';
 import styles from './TopbarMobile.module.scss';
 import { Link } from 'react-router-dom';
+
 import { FaTrash, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
+import CurrancyDisplay from '../../common/CurrancyDisplay/CurrancyDisplay';
+
 const TopbarMobile = () => {
+  const [isToggled, setIsToggeld] = useState(false);
   return (
-    <div className={styles.root}>
-      <section className={styles.dropdownChart}>
-        <div className={styles.chartContainer}></div>
-        <div className={styles.chartItems}></div>
-      </section>
-    </div>
+    <section className={styles.root}>
+      <div className={styles.chartContainer}>
+        <CurrancyDisplay containerClass={styles.currancyTag} amount={10000} />
+        {isToggled ? (
+          <FaChevronUp
+            className={styles.icon}
+            onClick={() => {
+              setIsToggeld(false);
+            }}
+          />
+        ) : (
+          <FaChevronDown
+            className={styles.icon}
+            onClick={() => {
+              setIsToggeld(true);
+            }}
+          />
+        )}
+      </div>
+      {isToggled && (
+        <div className={styles.chartItems}>
+          <ul className={styles.chartList}>
+            <li className={styles.chartItem}>
+              <div className={styles.planetContainer}></div>
+              <CurrancyDisplay
+                containerClass={styles.currancyTagItem}
+                amount={10000}
+              />
+              <div className={styles.trashIconContainer}>
+                <FaTrash className={styles.trashIcon} />
+              </div>
+            </li>
+          </ul>
+        </div>
+      )}
+    </section>
   );
 };
 
--- src/components/layout/DashboardHeader/DashboardHeader.test.js
+++ src/components/layout/DashboardHeader/DashboardHeader.test.js
@@ -3,6 +3,18 @@ import DashboardHeader from './DashboardHeader';
 import { shallow } from 'enzyme';
 
 describe('Component DashboardHeader', () => {
+  beforeEach(() => {
+    window.matchMedia = (query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    });
+  });
   it('Should render without crashing', () => {
     const component = shallow(<DashboardHeader />);
     expect(component).toBeTruthy();
